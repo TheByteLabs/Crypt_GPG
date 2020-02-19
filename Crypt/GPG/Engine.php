7debb7958f7cd35364abd6756c9d3584cae906b0
Olumide Omotoso <olumide@bytelabs.ng>
2020-02-19 07:37:47 +0100

    Update Engine.php

--- Crypt/GPG/Engine.php
+++ Crypt/GPG/Engine.php
@@ -1724,7 +1724,7 @@ class Crypt_GPG_Engine
             $this->_pipes,
             null,
             $env,
-            array('binary_pipes' => true)
+            null
         );
 
         if (!is_resource($this->_process)) {
